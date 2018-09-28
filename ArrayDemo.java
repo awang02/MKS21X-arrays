163b5761333048513167baf27be83b17d6ac9f06
Allen Wang <awang02@stuy.edu>
2018-09-28 02:10:20 -0400

    Changed functions to return instead of print

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,12 +1,12 @@
 public class ArrayDemo {
-// 0a, 0b
-  public static void printArray(int[]ary){
+// 0a
+  public static String printArray(int[]ary){
     String finStr = "[";
     for (int i = 0; i < ary.length; i++) {
       finStr += ary[i] + ", ";
     }
     finStr = finStr.substring(0,finStr.length()-2) + "]";
-    System.out.print(finStr);
+    return finStr;
   }
   //public static void printArray(int[][]ary){}
 
@@ -16,7 +16,6 @@ public class ArrayDemo {
     for (int i = 0; i < a.length;i++) {
       a[i] = i * i + 2;
     }
-    printArray(a);
-
+    System.out.println("0a:\n" + printArray(a));
   }
 }
